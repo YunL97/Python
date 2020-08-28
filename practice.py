d81ad950f6fa3f317008ae96c25d67c5b00a2743
leeyunsik <58182106+leeyunsik@users.noreply.github.com>
2020-08-28 15:13:52 +0900

    Add files via upload

--- practice.py
+++ practice.py
@@ -259,6 +259,88 @@ print("치킨당첨자 : {0}".format(winners[0]))
 print("커피당첨자 : {0}".format(winners[1:]))
 print("축하합니다")
 
+#if
+#weather = "ㅁ"
+weather = input("오늘 날씨는 어떄요?")
+if(weather) == "비" or weather == "눈":
+    print("우산을 챙기세요")
+elif weather == "미세먼지":
+    print("마스크를 챙기세요")
+else:
+    print("준비물 필요 없어요")
+
+temp = int(input("기온은 어떄요?"))
+if 30<=temp:
+    print("너무 더워요. 나가지 마세요")
+elif 10<+ temp and temp < 30:
+    print("괜찮은 날씨에요")
+elif 0<= temp and temp <10:
+    print("외투를 챙기세요")
+else:
+    print("너무 추워요. 나가지 마세요")
+
+
+#for
+for waiting_no in[0,1,2,3,4]:
+    print("대기번호 : {0}".format(waiting_no))
+
+for waiting_no in range(1,5): # 1,2,3,4
+    print("대기번호 : {0}".format(waiting_no))
+
+starbucks = ["아이언맨","토르","그루트"]
+for customer in starbucks:
+    print("{0},커피가 준비되었습니다".format(customer))
+
+#while
+customer = "토르"
+index = 5
+while index >= 1:
+    print("{0}, 커피가 준비 되엇습니다. {1} 번 남았어요".format(customer, index))
+    index -=1
+    if index ==0:
+        print("커피는 폐기 처분되엇습니다")
+
+# customer = "아이언맨"
+# index = 1
+# while True: #ctrl + c 강제종료
+#     print("{0}, 커피가 준비 되엇습니다. 호출 {1} 회".format(customer, index))
+#     index +=1
+
+person = "Unknown"
+while person != customer:
+    print("{0}, 커피가 준비되었습니다".format(customer))
+    person = input("이름이 어떻게 되세요?")
+
+#continu break
+absent = [2,5] #결석
+no_book = [7] #책을 깜빡했음
+for student in range(1,11): # 1,2,3,4,5,6,7,8,9,10
+    if student in absent : 
+        continue
+    elif student in no_book:
+        print("오늘 수업 여기싸지, {0}는 교무실로 다라와".format(student))
+        break
+    print("{0}, 책을 읽어봐".format(student))
+
+#한줄 for
+#출석번호가 1 2 3 4, 앞에 100을 붙이기로 함 -> 101,102,103,104
+students = [1,2,3,4,5]
+print(students)
+students = [i+100 for i in students]
+print (students)
+
+#학생 이름을 길이로 변환
+students = ["iron man", "thor", "i am groot"]
+students = [len(i) for i in students]
+print(students)
+
+#학생 이름을 대문자로 변환
+students = ["iron man", "thor", "i am groot"]
+students = [i.upper() for i in students]
+print(students)
+
+    
+
 
 
 
