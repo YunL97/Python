8238550d01f42b79828cc3d741826c15390ddfb7
leeyunsik <58182106+leeyunsik@users.noreply.github.com>
2020-08-31 19:32:06 +0900

    Add files via upload

--- practice.py
+++ practice.py
@@ -339,11 +339,54 @@ students = ["iron man", "thor", "i am groot"]
 students = [i.upper() for i in students]
 print(students)
 
-    
+
+#함수
+def open_account():
+    print("새로운 계좌가 생성되었습니다")
 
 
+open_account()
+
+def deposit(balance, money): #입금
+    print("입금이 완료되었습니다. 잔액은 {0}원입니다".format(balance + money))
+    return balance + money
+
+def withdraw(balance,money): #출금
+    if balance >= money: #잔액이 출금보다 많으면
+        print("출금이 완료되었습니다. 잔액은 {0}원입니다".format(balance-money))
+        return balance - money
+    else:
+        print("출금이 완료되지않았습니다.잔액은 {0}원입니다".format(balance))
+        return balance
+
+def withdraw_night(balance,money): #저녁에 출금
+    commission = 100 #수수료 100원
+    return commission,balance - money-commission
+balance = 0
+balance = deposit(balance,2000)
+print(balance)
+balance = withdraw(balance,2000)
+commission, balance = withdraw_night(balance,500)
+print("수수료 {0}원이며, 잔액은 {1}원 입니다".format(commission,balance))
+
+# def profile(name, age=17, main_lang="파이썬"):
+#     print("이름: {0}\t나이 : {1}\t주 사용 언어:{2}"\
+#         .format(name,age,main_lang))
+# profile("유재석")
+
+#가변인자
+def profile(name, age, *language):
+    print("이름 : {0}\t나이 : {1}\t".format(name,age),end=" ") #end=" " 줄바꿈x
+    #print(lang1,lang2,lang3,lang4,lang5)
+    for lang in language:
+        print(lang, end=" ")
+    print()
 
 
+profile("유재석",20,"파이선","자바","c","c++","c#")
+profile("김태호",25,"코틀린","스위프트","","","")
+
+#지역변수와 전역변수
 
 
 
