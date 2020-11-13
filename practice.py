e3db4deb89b644910773a51b63b3e22fa5c5356f
leeyunsik <58182106+leeyunsik@users.noreply.github.com>
2020-11-13 23:56:09 +0900

    Add files via upload

--- practice.py
+++ practice.py
@@ -710,12 +710,20 @@ from module import price, price_morning #가져다 쓸것만 할당 가능
 price(5)
 
 #패키지
-import travel.thailand
-trip_to = travel.thailand.ThailandPackage()
-trip_to.detail()
+# import travel.thailand
+# trip_to = travel.thailand.ThailandPackage()
+# trip_to.detail()
 
+# from travel.thailand import ThailandPackage
+# trip_to  = ThailandPackage()
+# trip_to.detail()
 
+# from travel import vietnam
+# trip_to = vietnam.VietnamPackage()
+# trip_to.detail
 
-
+from travel import *
+trip_to = vietnam.VietnamPackage()
+trip_to.detail
 
 
